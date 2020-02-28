5686c1f8b10f67f1e0ca5e3a8c0e0787699dc976
Kerbetth <57868057+Kerbetth@users.noreply.github.com>
2020-02-28 09:00:34 +0100

    correction et nettoyage

--- src/main/java/com/safetynet/safetynetalert/SafetyNetAlertApplication.java
+++ src/main/java/com/safetynet/safetynetalert/SafetyNetAlertApplication.java
@@ -3,6 +3,7 @@ package com.safetynet.safetynetalert;
 import org.springframework.boot.autoconfigure.*;
 import org.springframework.boot.*;
 import org.slf4j.*;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 public class SafetyNetAlertApplication
--- src/main/java/com/safetynet/safetynetalert/controllers/htmlcontrollers/IndexHTMLControllers.java
+++ src/main/java/com/safetynet/safetynetalert/controllers/htmlcontrollers/IndexHTMLControllers.java
@@ -21,5 +21,4 @@ public class IndexHTMLControllers {
         model.addAttribute("firestations", dao.loadFirestions());
         return "Index";
     }
-
 }
