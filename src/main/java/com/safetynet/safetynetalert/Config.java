769048f267f8617f000c0e0681fcb755dcd836f7
dufeu <kerbetth@protonmail.com>
2020-06-10 14:04:55 +0200

    ajout des retriver dans application.properties

---
+++ src/main/java/com/safetynet/safetynetalert/Config.java
@@ -0,0 +1,15 @@
+package com.safetynet.safetynetalert;
+
+import org.springframework.boot.actuate.trace.http.HttpTraceRepository;
+import org.springframework.boot.actuate.trace.http.InMemoryHttpTraceRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class Config {
+    @Bean
+    public HttpTraceRepository htttpTraceRepository()
+    {
+        return new InMemoryHttpTraceRepository();
+    }
+}
